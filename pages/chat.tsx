68f5e7b70b9a503fc4cca8716f2da3fa7611e395
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-11-14 22:04:26 +0100

    Update chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,21 +1,28 @@
 // pages/NeueSeite.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'components/Container'; 
 
 const NeueSeite: React.FC = () => {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const iframe = document.getElementById('themeChangerFrame');
+    if (iframe) {
+      iframe.contentWindow.postMessage({ theme: theme }, '*');
+    }
+  }, [theme]);
+
   return (
     <Container>
       <h1>Hallo von der neuen Seite!</h1>
-
-      <iframe src="https://togetherai-einfachllamistral.hf.space" style={{
-    position: "absolute",
-    top: 0,
-    left: 0,
-    height: "100%",
-    width: "100%",
-    border: 0
-}}></iframe>
-      
+      <iframe id="themeChangerFrame" src="https://togetherai-einfachllamistral.hf.space" style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        border: 0
+      }}></iframe>
     </Container>
   );
 };
