d3c07c0179a6c922704f68358f7400976f46679f
Alex <148038194+Einfachalf@users.noreply.github.com>
2023-11-14 21:03:43 +0100

    Update chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -6,6 +6,16 @@ const NeueSeite: React.FC = () => {
   return (
     <Container>
       <h1>Hallo von der neuen Seite!</h1>
+
+      <iframe src="https://togetherai-einfachllamistral.hf.space" style={{
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    border: 0
+}}></iframe>
+      
     </Container>
   );
 };
